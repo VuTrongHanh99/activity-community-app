541ffec5a00590759885b3c00f03bf63d2e16b97
VuTrongHanh99 <vuhanhcntt899@gmail.com>
2022-12-30 11:56:45 +0700

    add components login register main pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { MainComponent } from './Components/main/main.component';
+import { ChangePassComponent } from './Components/pages/change-pass/change-pass.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    MainComponent,
+    ChangePassComponent
   ],
   imports: [
     BrowserModule,
